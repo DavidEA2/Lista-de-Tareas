eb8252e31a95777c34a043828beb34a9fb33d7ae
DavidEA2 <94660486+DavidEA2@users.noreply.github.com>
2022-04-22 19:10:35 -0300

    Agregar mock de tareas y el interface de las mismas

---
+++ lista-tareas/src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text: "Terminar primer modulo de Angular",
+        day: 'Mayo a las 23:00',
+        reminder: true,
+    },
+    {
+        id:2,
+        text: "Terminar Modulo 8",
+        day: 'Mayo 23 a las 12:00',
+        reminder: true,
+    },
+    {
+        id:3,
+        text: "Terminar Empezar Modulo 9",
+        day: 'Mayo 24 a las 12:00',
+        reminder: false,
+    },
+    {
+        id:4,
+        text: "Terminar Portfolio",
+        day: 'Mayo 30 a las 12:00',
+        reminder: false,
+    },
+]
